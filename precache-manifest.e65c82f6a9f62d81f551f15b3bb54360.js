cc2208552e6f873cd9b294bd9cd692088cc24899
Mars <speed0939@gmail.com>
2021-07-23 12:08:07 +0800

    feat: first commit

---
+++ precache-manifest.e65c82f6a9f62d81f551f15b3bb54360.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e422cc33c179f7653bddfc3ca0b833b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d73da5e7e6c51fd1a660",
+    "url": "/static/js/2.0612a842.chunk.js"
+  },
+  {
+    "revision": "c08b4db4faf10caeadc483dd7784ddca",
+    "url": "/static/js/2.0612a842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b4df746cedcdb0d040",
+    "url": "/static/js/main.fe3e07db.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
